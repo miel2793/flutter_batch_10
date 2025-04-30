f49e4f0682b436427cf4fc5b4098a5673eaa359f
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-01 02:53:46 +0600

    /..

---
+++ bin/Module_3/Class_1/list_in_loop.dart
@@ -0,0 +1,10 @@
+main()
+{
+  List<String> student =['miel','misat','mahin','mubin','mahia'];
+  List<int> due=[5666,3444,666,4444,6753];
+
+  for(int i=0;i<student.length;i++)
+    {
+      print("$i no student name is : ${student[i]} yout deu is ${due[i]}");
+    }
+}
