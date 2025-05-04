834edf2a58d80443c1580e2abe40a340dbc7072d
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-04 14:19:54 +0600

    update while loop

---
+++ bin/Module_3/Class_1/while_loop.dart
@@ -0,0 +1,19 @@
+main()
+{
+   /*int a=1;
+   while(a<=10)
+     {
+       print(10*a);
+       a++;
+     }*/
+  List<String> a=['ahnam','mielk','imran','sifat'];
+  List<int> r=[4,6,8,2];
+  int c=0;
+
+  while(c<a.length)
+    {
+      print("${a[c]} your resutl is ${r[c]}");
+      c++;
+    }
+
+}
