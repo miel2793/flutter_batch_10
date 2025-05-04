971bc7d3bc855d578ab39e4f9f5c037941292a28
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-04 15:07:43 +0600

    function basic

---
+++ bin/Module_3/class_2/function_basic.dart
@@ -0,0 +1,17 @@
+main()
+{
+  print('area =${cl(5,6)}');
+  print('area =${cl(6,6)}');
+
+  print('area =${cl(5,8)}');
+
+  print('area =${cl(59,6)}');
+
+
+}
+double cl (double l, double r )
+
+{
+ double area=  l*r;
+ return area;
+}
