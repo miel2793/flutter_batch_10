defef53382c7c92ae67fed48f8455f93c11d0842
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-04 20:20:48 +0600

    ..

--- bin/Module_3/class_2/function_basic.dart
+++ bin/Module_3/class_2/function_basic.dart
@@ -1,37 +1,27 @@
 import 'dart:io';
 
-// miel 2793 
+// miel 2793
 
-
-main()
-{
-
-  double length=double.parse(stdin.readLineSync()!);
-  double width= double.parse(stdin.readLineSync()!);
+main() {
+  double length = double.parse(stdin.readLineSync()!);
+  double width = double.parse(stdin.readLineSync()!);
   //print('area =${calcculate_area(length,width)}');
- String ? m =stdin.readLineSync();
-calcculate_area_94(length,width,);
+  String? m = stdin.readLineSync();
+  calcculate_area_94(length, width);
   //5calcculate_area_94(length,width,'Dhaka');
-
-
 }
-double calcculate_area (double length , double width )
 
-{
- double area=  length *width;
- return area;
+double calcculate_area(double length, double width) {
+  double area = length * width;
+  return area;
 }
+
 //optional perameter
- calcculate_area_94 (double length , double width ,[String ? m])
-
-{
-  double area=  length *width;
-  if(m != null)
-    {
-      print("The Area of $m is $area");
-    }
-  else
-    {
-      print("Area is $area");
-    }
+calcculate_area_94(double length, double width, [String? m]) {
+  double area = length * width;
+  if (m != null) {
+    print("The Area of $m is $area");
+  } else {
+    print("Area is $area");
+  }
 }
