41c51546f734df99abb4c990bd3167e284323de3
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-04 20:07:33 +0600

    with optional perameter

--- bin/Module_3/class_2/function_basic.dart
+++ bin/Module_3/class_2/function_basic.dart
@@ -8,8 +8,9 @@ main()
 
   double length=double.parse(stdin.readLineSync()!);
   double width= double.parse(stdin.readLineSync()!);
-  print('area =${calcculate_area(length,width)}');
-
+  //print('area =${calcculate_area(length,width)}');
+  String ? m =stdin.readLineSync();
+calcculate_area_94(length,width,m);
 
 }
 double calcculate_area (double length , double width )
@@ -17,4 +18,18 @@ double calcculate_area (double length , double width )
 {
  double area=  length *width;
  return area;
+}
+//optional perameter
+ calcculate_area_94 (double length , double width ,[String ? m])
+
+{
+  double area=  length *width;
+  if(m != null)
+    {
+      print("The Area of $m is $area");
+    }
+  else
+    {
+      print("Area is $area");
+    }
 }
