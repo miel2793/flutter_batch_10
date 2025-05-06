52e352f3623940a01a21f2d80eaa8333babb6708
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-06 16:12:48 +0600

    .. ..

---
+++ bin/OOP/Class/creating_object.dart
@@ -0,0 +1,17 @@
+main()
+{
+   cutsomer customer1 = cutsomer();
+   customer1.name="Miel Mahmud Sifat";
+   customer1.id=256;
+   customer1.Adress="Mirpur";
+
+   print(customer1.name);
+}
+
+class cutsomer
+{
+    String ? name;
+    int ? id;
+     String ? Adress;
+
+}
