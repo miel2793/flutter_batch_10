3f2d3b63db3dd9e5b7ccb9b9c25db607c96e8bea
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-06 16:20:19 +0600

    with 2 customer

--- bin/OOP/Class/creating_object.dart
+++ bin/OOP/Class/creating_object.dart
@@ -1,14 +1,18 @@
 main()
 {
-   cutsomer customer1 = cutsomer();
+   customer customer1 = customer();
    customer1.name="Miel Mahmud Sifat";
    customer1.id=256;
    customer1.Adress="Mirpur";
-
-   print(customer1.name);
+   ////customer 2;
+   customer customer2  =customer();
+customer2.name="Hasan Ali";
+customer2.Adress="Dhaka";
+customer2.id=456;
+   print(customer2.name);
 }
 
-class cutsomer
+class customer
 {
     String ? name;
     int ? id;
