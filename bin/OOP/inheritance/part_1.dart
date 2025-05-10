0f137a2b703b9eb941a0ff2be38833176bc24ae8
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-10 12:45:01 +0600

    update inheritance

---
+++ bin/OOP/inheritance/part_1.dart
@@ -0,0 +1,36 @@
+main()
+{
+ father oj =father();
+  oj.Class='Akon';
+  oj.name ='Zakir Akon';
+  oj.age=48;
+  child_1 l=child_1();
+  l.name='Miel MAhmud';
+  l.age=23;
+  l.Class='Akon';
+  l.roll=279324;
+  l.display_2();
+}
+class father
+{
+  String ? name;
+    String ? Class;
+    int ? age;
+
+  display()
+  {
+    print("Name: $name\nage: $age\nClass:$Class");
+
+  }
+}
+class child_1 extends father
+{
+  int? roll;
+
+  display_2()
+  {
+    print("Name: $name\nage: $age\nClass:$Class\nID:$roll");
+
+  }
+}
+
