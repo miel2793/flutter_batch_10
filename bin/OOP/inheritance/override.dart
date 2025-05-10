2739d8e9fc11d7bbe6e1c00512652ffd7e846a04
MD: Miel Mahmud Sifat <mahmudsifat2793@gmail.com>
2025-05-10 13:10:58 +0600

    update inheritance

---
+++ bin/OOP/inheritance/override.dart
@@ -0,0 +1,36 @@
+main()
+{
+   child k= child();
+   k.display();
+
+}
+ class father
+ {
+   display()
+   {
+     print('My name is  hash');
+   }
+
+ }
+ class child extends father
+ {
+   @override
+  display() {
+ print('mile ');
+  }
+ }
+/*
+
+Overriding means redefining a method in the child class that already exists in the parent class.
+
+It allows a subclass to provide a specific implementation of a method that is
+already defined in its superclass.
+
+✅ Rules of Overriding in Dart:
+Method name must be the same.
+
+Method signature (parameters) must be the same.
+
+Use the @override annotation for clarity (not mandatory, but recommended).
+
+The child class must extend or implement the parent class.*/
